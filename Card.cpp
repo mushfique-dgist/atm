dd141e851de4052da4ff798104de72762bec6f7a
Mushfique <mushfique@example.com>
2025-11-02 22:26:43 +0900

    Update .gitignore and refactor Card class implementation

--- Card.cpp
+++ Card.cpp
@@ -1,21 +1,24 @@
 #include "Card.hpp"
 
-Card(const CardNumber& card_num,
-     const BankName& bank,
-     CardRole role = CardRole::User):
-     cardNumber(card_num), BankName(card_bank), CardRole(role){}
+Card::Card(const CardNumber& card_num,
+           const BankName& card_bank,
+           CardRole role_value)
+    : cardNumber(card_num),
+      bankName(card_bank),
+      role(role_value) {}
 
-const CardNumber& getNumber() const{
+const CardNumber& Card::getNumber() const {
     return cardNumber;
 }
 
-const BankName& getBank() const{
+const BankName& Card::getBank() const {
     return bankName;
 }
 
-CardRole getRole() const{
-    return role}
+CardRole Card::getRole() const {
+    return role;
+}
 
-bool isAdmin() const{
+bool Card::isAdmin() const {
     return role == CardRole::Admin;
-};
+}
